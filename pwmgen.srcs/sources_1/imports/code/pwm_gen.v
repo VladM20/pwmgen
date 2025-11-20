60723b64105b8cc3b2c86a55038fe7a723432ae6
unknown <teodora.victoria28@gmail.com>
2025-11-20 18:38:57 +0200

    Am realizat pwm_gen.v

--- pwmgen.srcs/sources_1/imports/code/pwm_gen.v
+++ pwmgen.srcs/sources_1/imports/code/pwm_gen.v
@@ -12,5 +12,51 @@ module pwm_gen (
     // top facing signals
     output pwm_out
 );
+
+    // Bit 0: Aliniere Stanga (0) / Dreapta (1)
+    wire F_ALIGN_LR = functions[0];
+    // Bit 1: Aliniat (0) / Nealiniat (1)
+    wire F_MODE_UNALIGNED = functions[1];
+    
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            pwm_out <= 1'b0;
+        end else if (pwm_en) begin
+
+            // Modul ALINIAT (FUNCTIONS[1] = 0)
+            if (F_MODE_UNALIGNED == 1'b0) begin
+                
+                // Aliniere Stânga (FUNCTIONS[0] = 0)
+                if (F_ALIGN_LR == 1'b0) begin
+                    // HIGH cât timp contorul este mai mic decât COMPARE1
+                    if (count_val < compare1)
+                        pwm_out <= 1'b1;
+                    else
+                        pwm_out <= 1'b0;
+        
+                // Aliniere Dreapta (FUNCTIONS[0] = 1)
+                end else begin 
+                    // HIGH pe ultima sectiune a perioadei (>= period - compare1)
+                    if (count_val < (period - compare1))
+                        pwm_out <= 1'b0;
+                    else
+                        pwm_out <= 1'b1;
+                end
+                
+            // Modul NEALINIAT (FUNCTIONS[1] = 1) 
+            end else begin 
+                
+                if (compare1 < compare2 && counter_val >= compare1 && counter_val < compare2) begin
+                    pwm_out <= 1'b1;
+                end else begin
+                    pwm_out <= 1'b0;
+                end
+
+            end 
+        end else begin
+            pwm_out <= 1'b0; 
+        end
+    end
     
 endmodule
+
