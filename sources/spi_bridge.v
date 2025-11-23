5aa59cd3d3e5d97a9f924a2db4c741d0ac711ffb
VladM20 <vlad.moisanu@gmail.com>
2025-11-23 17:41:54 +0200

    Add comments to spi_bridge.v

--- sources/spi_bridge.v
+++ sources/spi_bridge.v
@@ -13,13 +13,12 @@ module spi_bridge (
     input[7:0] data_out
 );
 
-<<<<<<< HEAD
-reg sclk_d1;
-reg sclk_d2;
+reg sclk_d1; // starea semnalului sclk la momentul actual
+reg sclk_d2; // starea semnalului sclk la momentul anterior
 
 always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-            sclk_d1 <= 1'b0;
+            sclk_d1 <= 1'b0; 
             sclk_d2 <= 1'b0;
         end 
         else begin
@@ -28,11 +27,11 @@ always @(posedge clk or negedge rst_n) begin
         end
 end
 
-wire sclk_rising  = (sclk_d1 == 1'b1) && (sclk_d2 == 1'b0);
-wire sclk_falling = (sclk_d1 == 1'b0) && (sclk_d2 == 1'b1);
+wire sclk_rising  = (sclk_d1 == 1'b1) && (sclk_d2 == 1'b0); // sclk a fost 0 si a devenit 1
+wire sclk_falling = (sclk_d1 == 1'b0) && (sclk_d2 == 1'b1); // sclk a fost 1 si a devenit 0
 
 // --- Registre interne ---
-reg [2:0] bit_cnt;      // Contor pentru cei 8 bi?i (0-7)
+reg [2:0] bit_cnt;      // Contor pentru cei 8 biti (0-7)
 reg [7:0] shift_reg_rx; // Registru de deplasare pentru recep?ie (MOSI)
 reg [7:0] shift_reg_tx; // Registru de deplasare pentru transmisie (MISO)
 
@@ -40,30 +39,34 @@ always @(posedge clk or negedge rst_n) begin
     if(!rst_n) begin
         miso <= 1'b0;
         byte_sync <= 1'b0;
-        data_in <= 1'b0;
+        data_in <= 8'b0;
         bit_cnt <= 3'd0;
         shift_reg_rx <= 8'b0;
         shift_reg_tx <= 8'b0;
     end
     else begin
-        byte_sync <= 1'b0;
+        byte_sync <= 1'b0; // Inceperea numararii
         if(cs_n) begin
-            bit_cnt <= 3'd7;
+            bit_cnt <= 3'd7; // Setam contorul
             miso <= 1'bZ;
             shift_reg_tx <= data_out;
         end
         else begin
+            // Scriere MISO
             if(sclk_falling) begin
                 miso <= shift_reg_tx[7];
-                shift_reg_tx <= {shift_reg_tx[6:0], 1'b0};
+                shift_reg_tx <= {shift_reg_tx[6:0], 1'b0}; // Scoatem MSB si lipim 0 ca LSB
             end
+            // Citire MOSI
             if(sclk_rising) begin
                 shift_reg_rx <= {shift_reg_rx[6:0], mosi};
-                if(bit_cnt == 3'd0) begin
-                    bit_cnt <= 3'd7;
-                    data_in <= {shift_reg_rx[6:0], mosi};
-                    byte_sync <= 1'b1;
-                    shift_reg_tx <= data_out;
+
+                if(bit_cnt == 3'd0) begin // Am terminat un byte
+                    bit_cnt <= 3'd7; // Resetam contorul
+                    data_in <= {shift_reg_rx[6:0], mosi}; // Salvam byte-ul primit
+                    byte_sync <= 1'b1; // Semnalam byte primit
+
+                    shift_reg_tx <= data_out; // Pregatim urmatorul byte pentru transmisie MISO
                 end
                 else begin
                     bit_cnt <= bit_cnt - 1'b1;
@@ -72,6 +75,4 @@ always @(posedge clk or negedge rst_n) begin
         end
     end
 end
-=======
->>>>>>> d4da8f8a5efe705891e635479e9a3bed58d2cfa4
 endmodule
